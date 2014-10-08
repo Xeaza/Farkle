d74f0a635c1b4debc3d26e031c2efde3f6864753
xeaza <taylorwrightsanson@gmail.com>
2014-10-08 13:25:24 -0500

    Created subclass of UILabel called DieLabel

---
+++ Farkle/DieLabel.m
@@ -0,0 +1,21 @@
+//
+//  DieLabel.m
+//  Farkle
+//
+//  Created by Taylor Wright-Sanson on 10/8/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import "DieLabel.h"
+
+@implementation DieLabel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
